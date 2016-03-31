0ff140a8d161eaaf60f5fa8affa734722f141a8b
Matthew Oltendorf <matthew@oltendorf.net>
2016-03-31 08:38:52 -0700

    Loading settings file successfully.
    Panic on failure. Need to build installer some undetermined time in the future to make a settings file and other needed things.

---
+++ src/main.rs
@@ -0,0 +1,36 @@
+extern crate iron;
+extern crate toml;
+
+use std::env;
+use std::fs::File;
+use std::io::prelude::*;
+
+use iron::prelude::*;
+use iron::status;
+
+fn main() {
+	let mut path = env::home_dir().unwrap();
+
+	path.push(".sequence");
+	path.push("settings.toml");
+
+	let mut file = match File::open(&path) {
+		Ok(file) => {
+			println!("Opening settings file: {}!", path.to_string_lossy());
+			file
+		},
+		Err(err) => panic!("No settings file found: {}!", path.to_string_lossy()),
+	};
+
+	let mut input = String::new();
+	file.read_to_string(&mut input).unwrap();
+
+	let value = toml::Parser::new(&input).parse().unwrap();
+	println!("{:?}", value);
+
+	fn hello_world(_: &mut Request) -> IronResult<Response> {
+		Ok(Response::with((status::Ok, "Hello Rust!")))
+	}
+
+//	Iron::new(hello_world).http("[::1]:8001").unwrap();
+}
