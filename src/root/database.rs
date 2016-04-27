66b43d773da6c8509f4f3789eddb72ea36d395e6
Matthew Oltendorf <matthew@oltendorf.net>
2016-04-27 08:39:18 -0700

    Add database connection code

---
+++ src/root/database.rs
@@ -0,0 +1,46 @@
+extern crate mysql;
+
+use super::Root;
+
+use self::mysql::Opts;
+use self::mysql::OptsBuilder;
+use self::mysql::Pool;
+
+pub struct Database {
+  pool: Pool
+}
+
+impl Database {
+  pub fn new(root: &Root) -> Database {
+    let settings = root.settings();
+
+    let mut builder = OptsBuilder::new();
+
+    if let Some(socket) = settings.lookup("database.socket").map(|v| v.as_str()) {
+      builder.unix_addr(socket);
+    } else {
+      // Can be optional.
+      let host = settings.lookup("database.host").and_then(|v| v.as_str().map(|v| v.to_string()));
+
+      // Must exist.
+      let port = settings.lookup("database.port").and_then(|v| v.as_integer()).unwrap_or(3306) as u16;
+
+      builder.ip_or_hostname(host).tcp_port(port);
+    }
+
+    // Can be optional.
+    let username = settings.lookup("database.username").and_then(|v| v.as_str());
+    let password = settings.lookup("database.password").and_then(|v| v.as_str());
+    let database = settings.lookup("database.database").and_then(|v| v.as_str());
+
+    builder.user(username).pass(password).db_name(database);
+
+    Database {
+      pool: Pool::new(Opts::from(builder)).unwrap()
+    }
+  }
+
+  pub fn pool(&self) -> &Pool {
+    &self.pool
+  }
+}
--- src/root/mod.rs
+++ src/root/mod.rs
@@ -1,11 +1,14 @@
+pub mod database;
 pub mod daemon;
 pub mod settings;
 
 use self::daemon::Daemon;
+use self::database::Database;
 use self::settings::Settings;
 
 pub struct Root {
   daemon: Option<Daemon>,
+  database: Option<Database>,
   settings: Option<Settings>
 }
 
@@ -13,13 +16,19 @@ impl Root {
   pub fn new() -> Root {
     let mut root = Root {
       daemon: None,
+      database: None,
       settings: None
     };
 
     root.settings = Some(Settings::new(&root));
+    root.database = Some(Database::new(&root));
     root.daemon = Some(Daemon::new(&root));
 
     root
+  }
+
+  pub fn database(&self) -> &Database {
+    &self.database.as_ref().unwrap()
   }
 
   pub fn daemon(&self) -> &Daemon {
